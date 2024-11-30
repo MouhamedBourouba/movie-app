d6f3020c312f772901c2a48adca140d5c4969aa4
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2024-11-30 08:55:17 +0100

    mobile frendly

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,23 +3,15 @@ import { NavLink } from "react-router-dom";
 
 function Navbar({ navigationItems }) {
   return (
-    <nav className="flex h-12 w-full
-      flex-row items-center justify-between bg-black">
-      <div className="ml-2 flex flex-row">
-        <img src="./src/assets/logo.svg" className="size-8 self-center" />
-        <h2 className="ml-1 self-center font-bold text-white">Movier</h2>
-      </div>
-
-      <div className="ml-5">
+    <nav className="flex h-12 w-full flex-row items-center justify-between bg-black px-3">
+      {/* Menu for larger screens */}
+      <div className="flex flex-row space-x-4">
         {navigationItems.map((item, index) => (
           <NavLink
             key={index}
             to={item.route}
             className={({ isActive }) =>
-              `mr-4 self-center text-base font-semibold max-sm:text-xs ${isActive
-                ? "text-red-400 underline underline-offset-4"
-                : "text-white"
-              } transition-all duration-200 ease-in-out hover:text-red-300 hover:underline max-sm:mr-2`
+              `text-base font-semibold text-white ${isActive ? "text-red-500 underline" : "hover:text-red-300"} transition-all duration-200 ease-in-out max-sm:text-xs`
             }
           >
             {item.name}
@@ -27,7 +19,10 @@ function Navbar({ navigationItems }) {
         ))}
       </div>
 
-      <img className="mr-4 size-5" src="./src/assets/search.svg" />
+      <img
+        className="block h-5 w-5 transition-all duration-200 ease-in-out max-sm:h-4 max-sm:w-4"
+        src="./src/assets/search.svg"
+      />
     </nav>
   );
 }
