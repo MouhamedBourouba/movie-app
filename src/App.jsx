27bfc01d6abec43cde9f6905e91e28c6304d7b40
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2024-12-04 12:01:15 +0100

    cp

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { RouterProvider } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
-import Navbar from "./components/navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 import { Outlet } from "react-router-dom";
-import Popular from "./pages/Popular.jsx";
-import NowPlaying from "./pages/NowPlaying.jsx";
-import TopRated from "./pages/TopRated.jsx";
-import Upcoming from "./pages/Upcoming.jsx";
+import LoadingCircle from "./components/LoadingCircle.jsx";
+import MovieList from "./components/MovieList.jsx";
+import { useState, useEffect } from "react";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
-      {
-        path: "/",
-        element: <Popular />,
-      },
-      {
-        path: "/now-playing",
-        element: <NowPlaying />,
-      },
-      {
-        path: "/top-rated",
-        element: <TopRated />,
-      },
-      {
-        path: "/upcoming",
-        element: <Upcoming />,
-      },
+      //{
+      //  path: "/",
+      //  element: <Root title={"Popular"}/>,
+      //},
+      //{
+      //  path: "/now-playing",
+      //  element: <Root title={"Now Playing"}/>,
+      //},
+      //{
+      //  path: "/top-rated",
+      //  element: <Root title={"Top Rated"}/>,
+      //},
+      //{
+      //  path: "/upcoming",
+      //  element: <Root title={"Upcomming"}/>,
+      //},
     ],
   },
 ]);
@@ -39,11 +38,39 @@ const navigationItems = [
   { route: "/upcoming", name: "Upcoming" },
 ];
 
+// Leaked intentionally
+const API_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZGNhNTNlYzgwNzZkNzJlMjUwZTY5NzE3MzZjYTU4NiIsIm5iZiI6MTczMjkwMDcwMi45NDQ2Nzk1LCJzdWIiOiI2NzQ5ZjVmZmIzZDNlYjkzM2JhMjY0MGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.VBF0u3Eh52wZV0NQRFRjMawa-JtLumxMUNP9JPsr2C8";
+const POPULAR_URL =
+  "https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=3&sort_by=popularity.desc";
+
 function Root() {
+  let [loading, setLoading] = useState(true);
+  let [movieData, setData] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(POPULAR_URL, {
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setData(res);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
-      <div className="px-4 sm:px-16 md:px-24 xl:px-35 2xl:px-52 transition-all mb-6">
+      <div className="xl:px-35 mb-6 px-4 transition-all sm:px-16 md:px-24 2xl:px-52">
         <Navbar navigationItems={navigationItems} />
+        {loading && <LoadingCircle />}
+        {!loading && movieData && <MovieList movieData={movieData} />}
         <Outlet />
       </div>
     </>
